8fb54cf305ea8ac123e93f770a695bcf12544964
pagidas <pagakrivos@gmail.com>
2020-05-23 19:46:00 +0100

    Adds swagger ui generator to the api

--- src/main/kotlin/me/kostasakrivos/demo/http4k/ItemRoutes.kt
+++ src/main/kotlin/me/kostasakrivos/demo/http4k/ItemRoutes.kt
@@ -9,12 +9,19 @@ import me.kostasakrivos.demo.http4k.service.ItemService
 import org.http4k.contract.contract
 import org.http4k.contract.openapi.ApiInfo
 import org.http4k.contract.openapi.v3.OpenApi3
+import org.http4k.core.Method.GET
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.FOUND
 import org.http4k.core.then
 import org.http4k.filter.DebuggingFilters
 import org.http4k.filter.ServerFilters
+import org.http4k.routing.ResourceLoader.Companion.Classpath
 import org.http4k.routing.RoutingHttpHandler
 import org.http4k.routing.bind
 import org.http4k.routing.routes
+import org.http4k.routing.static
+
+private val swaggerUiVersion = "3.25.3"
 
 object ItemRoutes {
     operator fun invoke(itemService: ItemService): RoutingHttpHandler {
@@ -35,6 +42,15 @@ object ItemRoutes {
 
         return ServerFilters.CatchLensFailure
             .then(DebuggingFilters.PrintRequestAndResponse())
-            .then(routes("/api" bind contract))
+            .then(routes(
+                "/api" bind routes(swaggerUi, contract)
+            ))
     }
 }
+
+private val swaggerUi: RoutingHttpHandler = routes(
+    "docs" bind GET to {
+        Response(FOUND).header("Location", "/api/docs/index.html?url=/api")
+    },
+    "/docs" bind static(Classpath("META-INF/resources/webjars/swagger-ui/$swaggerUiVersion"))
+)
